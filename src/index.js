570eeb1fd3f30ca77c0ddd84d3cc755a081ab43f
INJAM SNEHITH <injam.snehith@terralogic.com>
2021-01-25 10:54:28 +0530

    onclick onmouse

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+process.env.CI=false
 ReactDOM.render(
   <React.StrictMode>
     <App />
